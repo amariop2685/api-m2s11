ed5ef4538c9549dc0fb797f7abae0b01e73935a2
amariop2685 <acostap.marioa@gmail.com>
2023-10-31 19:41:28 -0300

    Tarjetas trae imagen, precio y descripción

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,1 +1,32 @@
 console.log("Entro index.js");
+
+let contenidoProductos = document.getElementById("contenidoProductos");
+
+fetch("https://dev4humans.com.mx/api/clases/productos", { method: "GET" })
+    .then(response => response.json())
+    .then(data => {
+        console.log("Promesa de resolvio");
+        console.log(data);
+        const rawData = data;
+        const productos = rawData.data.products;
+        console.log(productos);
+        productos.forEach(producto => {
+            contenidoProductos.innerHTML = contenidoProductos.innerHTML +
+            `<div class="col-md-12">
+            <div class="card mb-3">
+        <div class="row g-0">
+          <div class="col-md-4">
+            <img src="${producto.thumbnail}" class="img-fluid rounded-start" alt="...">
+          </div>
+          <div class="col-md-8">
+            <div class="card-body">
+              <h5 class="card-title">${producto.title}</h5>
+              <p class="card-text">${producto.description}</p>
+              <p class="card-text"><small class="text-body-secondary">$${producto.price}</small></p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>`;
+        });
+    }).catch(error => console.error(error))
